0ab456cc5d37d86ddcf29f0cdc882e2925f19011
Matviy <antipin.m9971@gmail.com>
2023-03-06 19:13:31 +0200

    modify video.js and feedback.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,30 +2,27 @@ import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {
+	
+}).catch(function (error) {
+	switch (error.name) {
+		case 'RangeError':
+			
+			break;
 
-player.on('play', function (data) {
-	player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {
-		// seconds = the actual time that the player seeked to
-	}).catch(function (error) {
-		switch (error.name) {
-			case 'RangeError':
-				// the time was less than 0 or greater than the video’s duration
-				break;
-
-			default:
-				// some other error occurred
-				break;
-		}
-	});
+		default:
+			
+			break;
+	}
 });
-// THIS STOP ME
+player.on('play', function (data) {
+	
+});
+
 player.on('timeupdate', throttle((data) => {
 	localStorage.setItem('videoplayer-current-time', data.seconds);
 	console.log(localStorage.getItem('videoplayer-current-time'));
 	}, 1000));
 
-// console.log(localStorage.getItem('videoplayer-current-time'))
-// player.getVideoTitle().then(function (title) {
-// 	console.log('title:', title);
-// });
+
 
