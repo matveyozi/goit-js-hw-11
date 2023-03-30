543ccc9f79c4d5db95db434ee9cab497798d8d02
Matviy <antipin.m9971@gmail.com>
2023-03-30 21:58:17 +0300

    modify apikey

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = `https://pixabay.com/api/?${process.env.PIX_KEY}/`;
+axios.defaults.baseURL = `https://pixabay.com/api/?/`;
 
 export const getData = async (search, page) => {
 	const { data } = await axios.get('https://pixabay.com/api/', {
 		params: {
 			q: search,
-			key: process.env.PIX_KEY,
+			key: '34711822-1a18608b89d6db278337710b9',
 			per_page: 20,
 			page,
 			image_type: 'photo',
